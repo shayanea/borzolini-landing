d46b206653742a760ca1e5e7be6a78af43c26086
Shayan Araghi <shayanaraghi@live.com>
2026-01-02 21:43:04 +0330

    chore: update sitemap with blog route

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -31,6 +31,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.8,
     },
     {
+      url: `${baseUrl}/blog`,
+      lastModified: currentDate,
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+    {
       url: `${baseUrl}/#coming-soon`,
       lastModified: currentDate,
       changeFrequency: "monthly",
